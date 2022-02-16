9e67c156811d86f95629d1a8977e76af37b5dd39
Berkin Düz <64802864+berkinduz@users.noreply.github.com>
2022-02-16 19:19:34 +0300

    rank message added

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,27 @@
 <template>
-  <p>Reaction Time is: {{ result }} ms</p>
+  <div>
+    <p>Reaction Time is: {{ result }} ms</p>
+    <h1>{{ rank }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["result"], //get from Parent
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.result < 250) {
+      this.rank = "OMG! You are FAST!";
+    } else if (this.result < 400) {
+      this.rank = "Not bad :>";
+    } else {
+      this.rank = "How old are you? 70?";
+    }
+  },
 };
 </script>
 
