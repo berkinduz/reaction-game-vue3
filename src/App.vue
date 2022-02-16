b4d55be55da492cf4bc7ed38d189812b10457264
Berkin Düz <64802864+berkinduz@users.noreply.github.com>
2022-02-16 18:57:32 +0300

    emitted reactionTime value setted to score

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Reaction Game</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-show="score !== null">Reaction Time: {{ score }}</p>
 </template>
 
 <script>
@@ -13,12 +14,17 @@ export default {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
     },
   },
 };
